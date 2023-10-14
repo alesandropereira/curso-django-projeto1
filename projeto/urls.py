35a7212273c4784f07ae481f77a2b2f1c76aeeb6
Alesandro Pereira <alesandro.pereira@gmail.com>
2023-10-13 22:39:02 -0300

    Teste com view simples

--- projeto/urls.py
+++ projeto/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def about_view(request):
+    return HttpResponse('retorno do request')
+    ...
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', about_view),
 ]
